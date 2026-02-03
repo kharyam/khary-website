66e952ea66b6bf4272f77687b25691ea5453647e
Khary Mendez <kharyam@gmail.com>
2026-02-03 13:20:20 -0500

    formatting

--- assets/js/resume-data.js
+++ assets/js/resume-data.js
@@ -314,12 +314,12 @@ const RESUME_DATA = {
     // Certifications
     certifications: {
         cloud: [
+            { name: "Certified Scrum Master", issuer: "Scrum Alliance", logo: "assets/images/scrum-logo.svg" },
             { name: "Certified Kubernetes Administrator (CKA)", issuer: "Cloud Native Computing Foundation", logo: "assets/images/cncf-logo.svg" },
             { name: "Certified Kubernetes Application Developer (CKAD)", issuer: "Cloud Native Computing Foundation", logo: "assets/images/cncf-logo.svg" },
             { name: "Certified Kubernetes Security Specialist (CKS)", issuer: "Cloud Native Computing Foundation", logo: "assets/images/cncf-logo.svg" },
             { name: "AWS Certified Cloud Practitioner", issuer: "Amazon Web Services", logo: "assets/images/aws-logo.svg" },
-            { name: "Certified Scrum Master", issuer: "Scrum Alliance", logo: "assets/images/scrum-logo.svg" },
-            { name: "FAA Certified Remote Pilot Certificate (Part 107)", issuer: "Federal Aviation Administration", logo: "assets/images/faa-logo.svg" }
+            { name: "FAA Certified Remote Pilot (Part 107)", issuer: "Federal Aviation Administration", logo: "assets/images/faa-logo.svg" }
         ],
         architect: [
             { name: "Red Hat Certified OpenShift Architect Level II", issuer: "Red Hat", date: "November 2024", featured: true, logo: "assets/images/redhat-logo.svg" },
@@ -539,7 +539,7 @@ const RESUME_DATA = {
                 icon: "📄",
                 iconType: "emoji",
                 label: "Resume",
-                description: "Printable PDF resume generator"
+                description: "Multi-format resume generator"
             }
         ]
     },
